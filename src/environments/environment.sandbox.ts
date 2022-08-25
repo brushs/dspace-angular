1a0cb0dc649133c8beaedde094ca10f0518cc043
Steve Brush <steve.brush@apption.com>
2022-08-25 12:11:05 -0400

    Env cleanup

--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -20,7 +20,7 @@ export const environment: Partial<GlobalConfig> = {
   // NOTE: these must be "synced" with the 'dspace.server.url' setting in your backend's local.cfg.
   rest: {
     ssl: true,
-    host: 'dspacebackendsteve.azurewebsites.net',
+    host: 'dspacesandboxbackend.azurewebsites.net',
     port: null,
     // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
     nameSpace: '/server',
